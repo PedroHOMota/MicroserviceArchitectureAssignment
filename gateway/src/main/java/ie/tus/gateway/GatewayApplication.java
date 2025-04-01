c5e936e45020e246a8951f4b9f4aed5c27d51f10
Pedro <PedroHOMota@users.noreply.github.com>
2025-04-01 11:10:45 +0100

    Adding gateway route

--- gateway/src/main/java/ie/tus/gateway/GatewayApplication.java
+++ gateway/src/main/java/ie/tus/gateway/GatewayApplication.java
@@ -2,12 +2,23 @@ package ie.tus.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator mainRoute(RouteLocatorBuilder routeLocatorBuilder){
+		return routeLocatorBuilder.routes().route(predicate ->
+			predicate.path("/**")
+				.uri("lb://APIREST")
+		).build();
 	}
 
 }
