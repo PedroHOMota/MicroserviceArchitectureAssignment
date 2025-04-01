3269cc7464382ff5ba7377ec1c3b0a4d5dc759f4
Pedro <PedroHOMota@users.noreply.github.com>
2025-04-01 08:24:09 +0100

    Moved configServer to proper packaging

--- config_server/src/main/java/ie/tus/ConfigServer/ConfigServerApplication.java
+++ config_server/src/main/java/ie/tus/ConfigServer/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.conf.ConfigServer;
+package ie.tus.ConfigServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
