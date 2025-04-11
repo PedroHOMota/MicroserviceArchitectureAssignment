72e38ae951f96942f9e3c5bad4ef63db012202a1
Pedro Mota <PedroHOMota@users.noreply.github.com>
2025-04-11 20:34:35 +0100

    Adding role based authorization

--- gateway/src/main/java/ie/tus/gateway/SecurityConfig.java
+++ gateway/src/main/java/ie/tus/gateway/SecurityConfig.java
@@ -29,10 +29,10 @@ public class SecurityConfig extends WebSecurityConfiguration {
     public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity serverHttpSecurity) throws Exception {
         serverHttpSecurity.authorizeExchange(authorizeExchangeSpec ->
             authorizeExchangeSpec.pathMatchers("/actuator/**").permitAll()
-//                    .pathMatchers(HttpMethod.POST,"/cookbook/**").hasRole("ADMIN")
-//                    .pathMatchers(HttpMethod.PUT,"/cookbook/**").hasRole("ADMIN")
-//                    .pathMatchers(HttpMethod.DELETE,"/cookbook/**").hasRole("ADMIN")
-                    .pathMatchers("/cookbook/**").authenticated()
+                   .pathMatchers(HttpMethod.POST,"/cookbook/**").permitAll()
+                    .pathMatchers(HttpMethod.PUT,"/cookbook/**").hasRole("ADMIN")
+                   .pathMatchers(HttpMethod.DELETE,"/cookbook/**").hasRole("ADMIN")
+                    .pathMatchers(HttpMethod.GET,"/cookbook/**").hasRole("ADMIN")
                     //.pathMatchers("/cookbook/**").permitAll()
 
                     .pathMatchers(HttpMethod.POST,"/recipes/**").hasRole("ADMIN")
@@ -41,8 +41,10 @@ public class SecurityConfig extends WebSecurityConfiguration {
                     .pathMatchers(HttpMethod.GET,"/recipes/**").authenticated()
                 //.pathMatchers("/recipes/**").authenticated()
             )
-            .oauth2ResourceServer(oAuth2ResourceServerSpec ->
-                oAuth2ResourceServerSpec.jwt(jwtSpec -> Customizer.withDefaults())
+                .oauth2ResourceServer(oAuth2ResourceServerSpec ->
+                                oAuth2ResourceServerSpec.jwt(jwtSpec -> jwtSpec.jwtAuthenticationConverter(authoritiesExtractor()))
+//            .oauth2ResourceServer(oAuth2ResourceServerSpec ->
+//                oAuth2ResourceServerSpec.jwt(jwtSpec -> Customizer.withDefaults())
             );
         serverHttpSecurity.csrf(csrfSpec -> csrfSpec.disable());
 
@@ -57,99 +59,3 @@ public class SecurityConfig extends WebSecurityConfiguration {
     }
 
 }
-
-//import java.util.Collection;
-//import java.util.List;
-//import java.util.Map;
-//import java.util.Optional;
-//import java.util.stream.Stream;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.core.convert.converter.Converter;
-//import org.springframework.security.authentication.AbstractAuthenticationToken;
-//import org.springframework.security.config.Customizer;
-//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.authority.SimpleGrantedAuthority;
-//import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
-//import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-//import org.springframework.security.oauth2.jwt.Jwt;
-//import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//@EnableWebSecurity
-//@EnableMethodSecurity
-//public class SecurityConfig {
-//
-//    interface AuthoritiesConverter extends Converter<Map<String, Object>, Collection<GrantedAuthority>> {}
-//
-//    @Bean
-//    AuthoritiesConverter realmRolesAuthoritiesConverter() {
-//        return claims -> {
-//            final var realmAccess = Optional.ofNullable((Map<String, Object>) claims.get("realm_access"));
-//            final var roles =
-//                realmAccess.flatMap(map -> Optional.ofNullable((List<String>) map.get("roles")));
-//            return roles.map(List::stream).orElse(Stream.empty()).map(SimpleGrantedAuthority::new)
-//                .map(GrantedAuthority.class::cast).toList();
-//        };
-//    }
-//
-//    @Bean
-//    JwtAuthenticationConverter authenticationConverter(
-//        Converter<Map<String, Object>, Collection<GrantedAuthority>> authoritiesConverter) {
-//        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-//        jwtAuthenticationConverter
-//            .setJwtGrantedAuthoritiesConverter(jwt -> authoritiesConverter.convert(jwt.getClaims()));
-//        return jwtAuthenticationConverter;
-//    }
-//
-//    @Bean
-//    SecurityFilterChain resourceServerSecurityFilterChain(HttpSecurity http,
-//                                                          Converter<Jwt, AbstractAuthenticationToken> jwtAuthenticationConverter) throws Exception {
-//        http.oauth2ResourceServer(resourceServer -> {
-//            resourceServer.jwt(jwtDecoder -> {
-//                jwtDecoder.jwtAuthenticationConverter(jwtAuthenticationConverter);
-//            });
-//        });
-//
-//        http.sessionManagement(sessions -> {
-//            sessions.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//        }).csrf(csrf -> {
-//            csrf.disable();
-//        });
-//
-//        http.authorizeHttpRequests(requests -> {
-//            requests.requestMatchers("/me").authenticated();
-//            requests.anyRequest().denyAll();
-//        });
-//
-//        return http.build();
-//    }
-//
-//    @Bean
-//    SecurityFilterChain clientSecurityFilterChain(
-//        HttpSecurity http,
-//        ClientRegistrationRepository clientRegistrationRepository) throws Exception {
-//        http.oauth2Login(Customizer.withDefaults());
-//        http.logout((logout) -> {
-//            var logoutSuccessHandler =
-//                new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
-//            logoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}/");
-//            logout.logoutSuccessHandler(logoutSuccessHandler);
-//        });
-//
-//        http.authorizeHttpRequests(requests -> {
-//            requests.requestMatchers("/actuator/gateway/routes", "/favicon.ico").permitAll();
-//            requests.requestMatchers("/**").hasAuthority("NICE");
-//            requests.anyRequest().denyAll();
-//        });
-//
-//        return http.build();
-//    }
-//}
